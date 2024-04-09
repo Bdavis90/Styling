20ff568643a465191e045a4c812d74b85c3ee13c
Brandon Davis <wowzerss90@yahoo.com>
2024-04-09 12:33:57 -0400

    Scoping css to components and using styled  components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,59 @@
+import {styled} from "styled-components"
 import logo from '../assets/logo.png';
-import './Header.css'
+// Scoping CSS to Component
+//import classes from './Header.module.css'
+
+// & tells styled components that parent is header
+const StyledHeaders = styled.header`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+margin-top: 2rem;
+margin-bottom: 2rem;
+
+& img {
+  object-fit: contain;
+  margin-bottom: 2rem;
+  width: 11rem;
+  height: 11rem;
+}
+
+& h1 {
+  font-size: 1.5rem;
+  font-weight: 600;
+  letter-spacing: 0.4em;
+  text-align: center;
+  text-transform: uppercase;
+  color: #9a3412;
+  font-family: 'Pacifico', cursive;
+  margin: 0;
+}
+
+& p {
+  text-align: center;
+  color: #a39191;
+  margin: 0;
+}
+
+@media (min-width: 768px) {
+  & {
+    margin-bottom: 4rem;
+  }
+
+  & h1 {
+    font-size: 2.25rem;
+  }
+}
+`
+
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeaders>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeaders>
   );
 }
